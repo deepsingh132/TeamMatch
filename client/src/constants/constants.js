8841992448c90b3f669f5e1840599690e9598b44
ManDeep Singh <mandeeparora132@gmail.com>
2023-02-22 01:22:34 +0530

    Delete client directory for deployment

--- client/src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-export const USERNAME_INITIAL_STATE = "";
-export const FULLNAME_INITIAL_STATE = "";
-export const TEAMNAME_INITIAL_STATE = "A";
-export const DESIGNATION_INITIAL_STATE = "";
-export const SKILLS_INITIAL_STATE = "";
-export const EXPERIENCE_INITIAL_STATE = 0;
-export const IMG_INITIAL_STATE = null;
-export const SUCCESS_INITIAL_STATE = false;
